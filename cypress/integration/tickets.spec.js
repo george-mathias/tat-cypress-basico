23a08e3474b687b75264be9ced95ee8af21928e9
George Mathias <gmathias80@gmail.com>
2021-09-28 21:50:45 -0300

    navegando e digitando

---
+++ cypress/integration/tickets.spec.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Tickets', () => {
+
+    beforeEach(() => cy.visit('https://bit.ly/2XSuwCW')) 
+
+    it.only('fills all the text input fields', () => {
+        const firstName = "George"
+        const lastName = "Mathias"
+
+        cy.url().should('contain', 'https://ticket-box.s3.eu-central-1.amazonaws.com/index.html')
+        cy.get('h1').should('contain', 'TICKETBOX')
+        cy.get('#first-name').type(firstName)
+        cy.get('#last-name').type(lastName)
+        cy.get('#email').type('mathias@gmail.com')
+        cy.get('#requests').type('Vegetarian')
+        cy.get('#signature').type(`${firstName} ${lastName}`)
+        cy.get('#agree').check()
+        cy.get('[type="submit"]').click()
+        cy.get('.success > p').should('contain', 'Ticket(s) successfully ordered.')
+    })
+
+    it("has 'ticketbox' header's heading", () => {});
+})
